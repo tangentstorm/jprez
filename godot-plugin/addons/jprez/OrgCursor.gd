487ca9128f3f2d4fce7b81be6b6e965fd018507b
tangentstorm <michal.wallace@gmail.com>
2022-02-28 22:16:57 -0500

    OrgCursor.gd: find_next(track), this_chunk()

--- godot-plugin/addons/jprez/OrgCursor.gd
+++ godot-plugin/addons/jprez/OrgCursor.gd
@@ -1,11 +1,12 @@
 tool class_name OrgCursor extends Reference
 
-enum S { ENTER, DESCEND, CHUNKS, RETURN, BREAK }
+enum S { ENTER, DESCEND, CHUNKS, NEXT, RETURN, BREAK }
 
 # tool to iterate through the nodes
 var node: OrgNode
 var stack: Array
-var step : int = 0
+var count: int = 0 # global count through the whole tree
+var step : int = 0 # local count within the current node
 var state : int = S.ENTER
 var head : String
 
@@ -13,6 +14,10 @@ func _init(root:OrgNode):
 	self.stack = []
 	self.node = root
 	self.state = S.ENTER
+
+func this_chunk()->OrgChunk:
+	if not (state == S.NEXT or state == S.CHUNKS): return null
+	return node.chunks[step] if step < len(node.chunks) else null
 
 func next_chunk()->OrgChunk:
 	var res = null
@@ -33,10 +38,20 @@ func next_chunk()->OrgChunk:
 					node = frame[0]; step = frame[1]; state = frame[2]
 				else: state = S.BREAK
 			S.CHUNKS:
-				if step < len(node.chunks):
-					res = node.chunks[step]
-					step += 1
-					break
+				res = this_chunk()
+				if res:
+					# yield before incrementing, so that this_chunk()
+					# can use the counters to retrieve the current chunk.
+					state = S.NEXT; break
 				else: state = S.RETURN
+			S.NEXT:
+				step += 1; count += 1
+				state = S.CHUNKS
 			S.BREAK: break
+	return res
+
+func find_next(track)->OrgChunk:
+	var res = next_chunk()
+	while res and res.track != track:
+		res = next_chunk()
 	return res

b0bc1ff2194878a05a9ecb0406ad815f55cc7547
tangentstorm <michal.wallace@gmail.com>
2022-12-20 23:52:39 -0500

    orgprez: fix display of the main scene

--- godot-plugin/addons/orgprez/plugin.gd
+++ godot-plugin/addons/orgprez/plugin.gd
@@ -8,6 +8,7 @@ var app # member variable holding instance of scene
 func _enter_tree():
 	app = scene.instance()
 	get_editor_interface().get_editor_viewport().add_child(app)
+	make_visible(false) # otherwise it shows up on-screen no matter what tab is active
 
 func has_main_screen():
 	return true
@@ -17,6 +18,9 @@ func get_plugin_name():
 
 func get_plugin_icon():
 	return get_editor_interface().get_base_control().get_icon("AutoKey", "EditorIcons")
+
+func make_visible(x): # called at startup and when the tab is changed
+    if app: app.visible = x
 
 func _exit_tree():
 	if app: app.queue_free()

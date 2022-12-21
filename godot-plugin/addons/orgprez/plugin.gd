616f5a3a02a424131378a73e7a7774744074e41d
tangentstorm <michal.wallace@gmail.com>
2022-12-20 23:26:17 -0500

    orgprez: add main screen tab

--- godot-plugin/addons/orgprez/plugin.gd
+++ godot-plugin/addons/orgprez/plugin.gd
@@ -2,10 +2,22 @@
 tool
 extends EditorPlugin
 
+var scene = preload('res://addons/orgprez/plugin.tscn')
+var app # member variable holding instance of scene
 
 func _enter_tree():
-	pass
+	app = scene.instance()
+	get_editor_interface().get_editor_viewport().add_child(app)
 
+func has_main_screen():
+	return true
+
+func get_plugin_name():
+	return "OrgPrez" # used in top-level tab name
+
+func get_plugin_icon():
+	return get_editor_interface().get_base_control().get_icon("AutoKey", "EditorIcons")
 
 func _exit_tree():
-	pass
+	if app: app.queue_free()
+

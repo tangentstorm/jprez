ef57c0f5d87bbb9346eb9cdda794c47d3e89f331
tangentstorm <michal.wallace@gmail.com>
2022-12-21 00:10:06 -0500

    whitespace fix

--- godot-plugin/addons/orgprez/plugin.gd
+++ godot-plugin/addons/orgprez/plugin.gd
@@ -20,7 +20,7 @@ func get_plugin_icon():
 	return get_editor_interface().get_base_control().get_icon("AutoKey", "EditorIcons")
 
 func make_visible(x): # called at startup and when the tab is changed
-    if app: app.visible = x
+	if app: app.visible = x
 
 func _exit_tree():
 	if app: app.queue_free()

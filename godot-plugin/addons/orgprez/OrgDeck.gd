9719d3c6132fa5f1998ad4b581b8c6e426ebbf3a
tangentstorm <michal.wallace@gmail.com>
2022-12-23 17:53:33 -0500

    orgprez: added OrgDeck script

---
+++ godot-plugin/addons/orgprez/OrgDeck.gd
@@ -0,0 +1,25 @@
+tool
+class_name OrgDeck extends Node
+
+func add_child(node:Node, legible_unique_name:bool=false):
+	.add_child(node, legible_unique_name)
+	node.connect("visibility_changed", self, "on_child_visibility_changed", [node])
+	show_only(node)
+
+# TODO: if the visible slide is removed, 
+#       and there is another slide, show one.
+#func remove_child(node:Node):
+#	if node.visible
+#	.remove_child(node)
+
+func on_child_visibility_changed(child):
+	if child.visible:
+		show_only(child)
+	
+func show_only(node:Node):
+	for child in self.get_children():
+		if node != child:
+			child.visible = false
+	if not node.visible:
+		node.visible = true
+

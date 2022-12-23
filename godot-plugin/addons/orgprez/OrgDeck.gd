b40188d6e0aaeb7f8fa93ebda7ac19c58471416d
tangentstorm <michal.wallace@gmail.com>
2022-12-23 18:42:20 -0500

    OrgDeck: observe child visibility on _ready

--- godot-plugin/addons/orgprez/OrgDeck.gd
+++ godot-plugin/addons/orgprez/OrgDeck.gd
@@ -3,8 +3,15 @@ class_name OrgDeck extends Node
 
 func add_child(node:Node, legible_unique_name:bool=false):
 	.add_child(node, legible_unique_name)
-	node.connect("visibility_changed", self, "on_child_visibility_changed", [node])
+	observe(node)
 	show_only(node)
+
+func _ready():
+	for child in get_children():
+		observe(child)
+
+func observe(node):
+	node.connect("visibility_changed", self, "on_child_visibility_changed", [node])
 
 # TODO: if the visible slide is removed, 
 #       and there is another slide, show one.

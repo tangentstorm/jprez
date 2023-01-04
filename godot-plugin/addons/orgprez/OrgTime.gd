5cab9e62f2899f4a028005182b60544e2382fdaf
tangentstorm <michal.wallace@gmail.com>
2023-01-03 23:00:04 -0500

    moved orgprez to @tangentstorm/orgprez

--- godot-plugin/addons/orgprez/OrgTime.gd
+++
@@ -1,9 +0,0 @@
-tool class_name OrgTime extends Resource
-# represents a timestamp. This is actually not
-# the org-mode format, but rather a format inspired
-# by subtitle files. hh:mm:ss.mss
-
-export var hh: int = 0
-export var mm: int = 0
-export var ss: int = 0
-export var ms: float = 0.0
